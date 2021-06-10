fb3e8803c14ac83007759a84ab863b2b84115963
Barsik04 <nsuzuki@maill.ru>
2021-06-10 13:26:25 +0300

    maybe work

--- src/test/java/Others/LoginPage.java
+++ src/test/java/Others/LoginPage.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage {
     WebDriver driver;
-    static private final String username = "89270054472";
-    static private final String password = "4815162342Nikita";
+    static private final String username = "";
+    static private final String password = "";
 
     static final String LOGIN_LOCATOR = ".//*[contains(@class, 'form')]/div/input[@type='text']";
     static final String PASSWORD_LOCATOR = ".//*[contains(@class, 'form')]/div/input[@type='password']";
